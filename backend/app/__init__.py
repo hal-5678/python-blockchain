06bc38c84f2de73ff1e884c74755b9abd3e4f83b
hal-5678 <59104822+hal-5678@users.noreply.github.com>
2020-01-14 08:09:45 -0500

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,38 @@
-from flask import Flask
+import os
+import random
+from flask import Flask, jsonify
+
+from backend.blockchain.blockchain import Blockchain
+from backend.pubsub import PubSub
 
 app = Flask(__name__)
+blockchain = Blockchain()
+pubsub = PubSub()
+
+@app.route('/')
+def route_default():
+    return 'Welcome to the Blockchain'
+
+@app.route('/blockchain')
+def route_blockchain():
+    return jsonify(blockchain.to_json())
+
+@app.route('/blockchain/mine')
+def route_blockchain_mine():
+    transaction_data = 'stubbed_transaction_data'
+
+    blockchain.add_block(transaction_data)
+
+    block = blockchain.chain[-1]
+
+    pubsub.broadcast_block(block)
+
+    return jsonify(block.to_json())
+
+PORT = 5000
+
+if os.environ.get('PEER') == 'True':
+    PORT = random.randint(5001,6000)
 
 
-@app.route('/test')
-def test():
-    return 'Test'
-
-app.run
+app.run(port=PORT)
